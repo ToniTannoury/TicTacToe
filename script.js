81c89b255e25b733a3d74074c70ff33c66dfd746
ToniTannoury <tatannoury01@gmail.com>
2023-07-12 23:05:35 +0300

    Creating the function that checks for the inner

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+const winningCombinations = [
+  [1, 2, 3], 
+  [4, 5, 6], 
+  [7, 8, 9], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [3, 6, 9], 
+  [1, 5, 9], 
+  [3, 5, 7]  
+]
+
 let selectedNumbers = {}
 let player = 1
 let board = document.getElementById("game-board")
 let line = document.getElementById("line")
 let resetGameButton = document.getElementById("resetButton")
 let resetScoresButton = document.getElementById("resetScoresButton")
+
+
+function checkWin(playerNumbers) {
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const combination = winningCombinations[i]
+    let hasWon = true
+    for (let j = 0; j < combination.length; j++) {
+      const number = combination[j]
+      if (!playerNumbers?.includes(number)) {
+        hasWon = false
+        break
+      }
+    }
+    if (hasWon) {
+      return combination
+    }
+  }
+  return null
+}
 
 function handleClickOnBoard(e) {
   if(e.target.id === "line"){
@@ -21,7 +51,13 @@ function handleClickOnBoard(e) {
     selectedNumbers[player] = selectedNumbers[player] || []
     selectedNumbers[player].push(+number)
 
-    
+    const winningCombination = checkWin(selectedNumbers[player])
+    if (winningCombination) {
+      console.log('Player', player, 'wins!')
+      line.classList = 'line'
+      return
+    }
+  
     if(player === 1){
       player = 2
     }else{
@@ -29,3 +65,4 @@ function handleClickOnBoard(e) {
     }
   }
 }
+board.addEventListener('click', handleClickOnBoard)
